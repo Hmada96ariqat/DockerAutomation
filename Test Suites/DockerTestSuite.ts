c5266ac55d173cf834fd48f76f0bac353b139aff
a.areiqat <a.areiqat@areiqat-pc.pio-tech.com>
2021-08-03 10:18:52 +0300

    TestSuite

---
+++ Test Suites/DockerTestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DockerTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>82db6475-51f5-40a3-b029-e581e545d553</testSuiteGuid>
+   <testCaseLink>
+      <guid>929323fb-2ccf-4b09-9a46-dfcaf3837561</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
